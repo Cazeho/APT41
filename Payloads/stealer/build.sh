681457c6f4c50345e0d80c45dd23e7058debfd76
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-02-14 15:15:18 +0100

    Update build.sh

--- Payloads/stealer/build.sh
+++ Payloads/stealer/build.sh
@@ -7,3 +7,7 @@ dotnet add package System.Data.SQLite
 dotnet add package System.Management
 dotnet add package System.Security.Cryptography.ProtectedData
 dotnet publish -c Release -r win-x64 --self-contained true
+
+
+
+wmic csproduct uuid
