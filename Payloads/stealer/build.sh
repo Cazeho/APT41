bc9ff13184aabb80d6de651129947024913ea67f
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-02-14 14:36:08 +0100

    Update build.sh

--- Payloads/stealer/build.sh
+++ Payloads/stealer/build.sh
@@ -2,4 +2,5 @@ dotnet new console -n stealer
 cd stealer
 dotnet add package System.Data.SQLite
 dotnet add package System.Management
+dotnet add package System.Security.Cryptography
 dotnet publish -c Release -r win-x64 --self-contained true
